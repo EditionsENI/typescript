f673ed8ce9ed265668f05bfdecba5014aba7ddda
Sylvain Pontoreau <pontoreau.sylvain@gmail.com>
2023-02-08 16:59:56 +0100

    🔇 Remove logs

--- v2/src/core/ioc/inject.ts
+++ v2/src/core/ioc/inject.ts
@@ -3,8 +3,6 @@ import { DependencyCollection } from "./dependencyCollection";
 export const Inject = <TClass, TValue>(key?: string) => {
   return (target: undefined, { name }: ClassFieldDecoratorContext<TClass, TValue>) => {
     return function(this: TClass, value: TValue) {    
-      console.log(key ?? name.toString().replace('#', '').toUpperCase());
-      console.log(value);
       return DependencyCollection.getInstance().get<TValue>(key ?? name.toString().replace('#', '').toUpperCase());
     }
   };
